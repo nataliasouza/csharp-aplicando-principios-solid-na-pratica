84e37e25b4324d01a0f631f075710017a41ac17c
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-22 20:03:55 -0300

    Named arguments

---
+++ pratica_poo/argumentos-nomeados/argumentos-nomeados/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace argumentos_nomeados
+{
+    class Program
+    {
+        static void Send(string destination, string title, string subject)
+        {
+            Console.WriteLine($"{destination} - {title} - {subject}");
+        }
+        static void Main(string[] args)
+        {
+            Send(destination: "Destino: nataliasouza.ti@hotmail.com", title: "Título: Argumentos Nomeados",
+                subject: "Assunto: Apenas treinando Argumentos Nomeados");
+
+            Send(title: "Título: Prática de Argumentos Nomeados", subject: "Assunto: Prática de Argumentos Nomeados", 
+                destination: "Destino: nattsouza.ti@hotmail.com");
+
+            Send(subject: "Assunto: Criar Artigo Argumentos Nomeados", destination: "Destino: natt.ti@hotmail.com",
+                title: "Título: Artigo Argumentos Nomeados");
+
+            Console.ReadLine();
+        }
+    }
+}
