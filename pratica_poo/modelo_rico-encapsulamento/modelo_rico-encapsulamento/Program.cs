6f349c78b713c4db59bfd895c6870506184fc9c1
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-22 20:55:22 -0300

    Rename method

--- pratica_poo/modelo_rico-encapsulamento/modelo_rico-encapsulamento/Program.cs
+++ pratica_poo/modelo_rico-encapsulamento/modelo_rico-encapsulamento/Program.cs
@@ -10,24 +10,24 @@ namespace modelo_rico_encapsulamento
             Client client = new Client(1,"Natt Souza"," Rua fulano de tal");
             Console.WriteLine($"Id: {client.Id} Cliente: {client.Name} Endereço:{client.Address}");           
 
-            var resultSum = Sum(2, 2, 3, 7);           
+            var resultSum = Addition(2, 2, 3, 7);           
             Console.WriteLine($"\nA soma dos 4 valores é: {resultSum}");
 
-            var resultSum2 = Sum(new int[] { 1, 2, 3, 4 });
+            var resultSum2 = Addition(new int[] { 1, 2, 3, 4 });
             Console.WriteLine($"A soma dos 4 valores é: {resultSum2}");
 
-            var resultSum3 = Program.Sum(2, 1, 10);
+            var resultSum3 = Program.Addition(2, 1, 10);
             Console.WriteLine($"A soma dos 3 valores é: {resultSum3}");
 
-            var resultSum4 = Sum(3, 1);
+            var resultSum4 = Addition(3, 1);
             Console.WriteLine($"A soma dos valores é: {resultSum4}\n");
 
             //using optional parameters
 
-            var result = Sum(100);
-            var result2 = Sum(100, 200);
-            var result3 = Sum(100, 300, 500);
-            var result4 = Sum(100, 200, 300, 400);
+            var result = Addition(100);
+            var result2 = Addition(100, 200);
+            var result3 = Addition(100, 300, 500);
+            var result4 = Addition(100, 200, 300, 400);
 
             Console.WriteLine($"Soma com 1 paramêtro{result}");
             Console.WriteLine($"Soma com 2 paramêtros{result2}");
@@ -38,22 +38,22 @@ namespace modelo_rico_encapsulamento
         }
 
         //Methods with overload
-        static public int Sum(int n1, int n2)
+        static public int Addition(int n1, int n2)
         {
             return n1 + n2;
         }
 
-        static public double Sum(double n1, int n2)
+        static public double Addition(double n1, int n2)
         {
             return n1 + n2;
         }
 
-        static public int Sum(int n1, int n2, int n3)
+        static public int Addition(int n1, int n2, int n3)
         {
             return (n1 + n2) + n3;
         }
 
-        static public int Sum(params int[] numbers)
+        static public int Addition(params int[] numbers)
         {
             var result = 0;
             foreach(var num in numbers)
@@ -62,7 +62,7 @@ namespace modelo_rico_encapsulamento
         }
 
         //Method - Parameters Optional
-        static int Sum(int n1, int n2 = 20, int n3 = 50, int n4 = 10)
+        static int Addition(int n1, int n2 = 20, int n3 = 50, int n4 = 10)
         {
             return n1 + n2 + n3 + n4;
         }
