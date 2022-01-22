125900bbc928dea84a343d246e98f0e403352872
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-22 19:18:22 -0300

    Poo practice: rich models (encapsulation) and methods with overload

---
+++ pratica_poo/modelo_rico-encapsulamento/modelo_rico-encapsulamento/Client.cs
@@ -0,0 +1,20 @@
+﻿namespace modelo_rico_encapsulamento
+{
+    public class Client
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Address { get; private set; }
+
+        public Client(int id, string name, string address)
+        {
+            DomainExceptionValidation.When(id < 0, "O id não pode ser negativo");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),"É necessário preencher o nome");
+            DomainExceptionValidation.When(string.IsNullOrEmpty(address),"Informe o indereço");
+
+            Id = id;
+            Name = name;
+            Address = address;
+        }
+    }
+}
---
+++ pratica_poo/modelo_rico-encapsulamento/modelo_rico-encapsulamento/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace modelo_rico_encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client(1,"Natt Souza"," Rua fulano de tal");
+            Console.WriteLine($"Id: {client.Id} Cliente: {client.Name} Endereço:{client.Address}");
+           
+
+            var resultSum = Sum(2, 2, 3, 7);           
+            Console.WriteLine($"A soma dos 4 valores é: {resultSum}");
+
+            var resultSum2 = Sum(new int[] { 1, 2, 3, 4 });
+            Console.WriteLine($"A soma dos 4 valores é: {resultSum2}");
+
+            var resultSum3 = Program.Sum(2, 1, 10);
+            Console.WriteLine($"A soma dos 3 valores é: {resultSum3}");
+
+            var resultSum4 = Sum(3, 1);
+            Console.WriteLine($"A soma dos valores é: {resultSum4}");
+
+            Console.ReadLine();
+        }
+
+        //trabalhando com sobrecargas
+        static public int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        static public double Sum(double n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        static public int Sum(int n1, int n2, int n3)
+        {
+            return (n1 + n2) + n3;
+        }
+
+        static public int Sum(params int[] numbers)
+        {
+            var result = 0;
+            foreach(var num in numbers)
+                result += num;
+            return result;
+        }
+    }
+}
