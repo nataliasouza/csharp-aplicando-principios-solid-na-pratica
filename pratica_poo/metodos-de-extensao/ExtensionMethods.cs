c56db986f5a940c0523fe049d41418bb48b3ee7e
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-22 22:30:57 -0300

    Extension Method

---
+++ pratica_poo/metodos-de-extensao/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿namespace metodos_de_extensao
+{
+    public static class ExtensionMethods
+    {
+        public static string UppercaseFirstLetter(this string value)
+        {
+            if(value.Length >0)
+            {
+                char[] array = value.ToCharArray();
+                array[0] = char.ToUpper(array[0]);
+                return new string(array);
+            }
+            return value;
+        }
+    }
+}
---
+++ pratica_poo/metodos-de-extensao/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace metodos_de_extensao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text1 = "curso";
+            var text2 = "cSharp";
+
+            //Extension Method
+            text1 = text1.UppercaseFirstLetter();
+            text2 = text2.UppercaseFirstLetter();
+
+            Console.WriteLine(text1);
+            Console.WriteLine(text2);
+
+        }
+    }
+}
