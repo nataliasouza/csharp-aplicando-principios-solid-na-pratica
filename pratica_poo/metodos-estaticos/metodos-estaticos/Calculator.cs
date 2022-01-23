7234c58ada662c56ce0e98623fe1ef6126ac4837
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-22 21:09:25 -0300

    Method Static

---
+++ pratica_poo/metodos-estaticos/metodos-estaticos/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace metodos_estaticos
+{
+    public class Calculator
+    {
+        public static int Addition(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public static int Subtract(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+    }
+}
---
+++ pratica_poo/metodos-estaticos/metodos-estaticos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace metodos_estaticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Soma = " + Calculator.Addition(10,3));
+            Console.WriteLine("Subtração = " + Calculator.Subtract(10, 5));
+
+            Console.ReadLine();
+        }
+    }
+}
