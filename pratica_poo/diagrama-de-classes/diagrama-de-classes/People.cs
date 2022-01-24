f88d3db699ae7b9c49895b15233f07483ce94b05
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-24 20:58:43 -0300

    Generating clss diagram

---
+++ pratica_poo/diagrama-de-classes/diagrama-de-classes/People.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace diagrama_de_classes
+{
+    public class People
+    {
+        private DateTime birthDate = new DateTime(1984, 01, 12); 
+        public string  Name { get; set; }
+        public int Age { get; set; }
+        public string Sex { get; set; }
+
+        public People()
+        {}
+
+        public People(string name, int age, string sex)
+        {            
+            Name = name;
+            Age = age;
+            Sex = sex;
+        }
+
+        public void IdentifyPerson()
+        {
+            Console.WriteLine($"Nome: {Name} - Idade:{Age} - Sexo: {Sex}");
+        }
+    }
+}
---
+++ pratica_poo/diagrama-de-classes/diagrama-de-classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace diagrama_de_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
