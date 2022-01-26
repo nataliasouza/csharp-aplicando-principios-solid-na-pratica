d2c42cd83ed11b985779ea40f4b5d1905b852c0b
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-25 21:03:18 -0300

    uml-agregacao

---
+++ pratica_poo/uml-agregacao/uml-agregacao/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace uml_agregacao
+{
+    public class Department : IDisposable
+    {
+        private string Name { get; set; }        
+        public List<Teacher> Teachers { get; set; }
+
+        public void Dispose()
+        { }
+    }
+}
---
+++ pratica_poo/uml-agregacao/uml-agregacao/Teacher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace uml_agregacao
+{
+    public class Teacher : IDisposable
+    {
+        public string Name { get; set; }       
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
