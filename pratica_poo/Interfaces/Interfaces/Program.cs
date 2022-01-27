4c8134f0aea70babdd3cee0f3cd79cde49ff7c63
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-01-26 23:39:29 -0300

    Interfaces

--- pratica_poo/Interfaces/Interfaces/Program.cs
+++ pratica_poo/Interfaces/Interfaces/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Interfaces.interfaces;
+using System;
 
 namespace Interfaces
 {
@@ -9,6 +10,11 @@ namespace Interfaces
             var saveDesign = new SaveDesing();            
             saveDesign.NameDesignForm(); //sobrescreve o método da classe base
             saveDesign.SaveDesignForm(); //método da interface
+
+            //usando interfaces
+            ISaveDesign isave = new SaveDesing();
+            isave.SaveDesignForm();
+            //isave.NameDesignForm();
         }
     }
 }
